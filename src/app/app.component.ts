f87ca90b0fc5cad716cde970bfc7cf43b33d075b
theetha1608 <theerthakunjusss@gmail.com>
2024-04-18 20:10:35 +0530

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,17 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Angular-Sweetalert';
+  handleClick() {
+    Swal.fire({
+      title: "Good job!",
+      text: "You clicked the button!",
+      icon: "success",
+      customClass: {
+        popup: 'custom-popup',
+      },
+      confirmButtonColor: 'green'
+    });
+    console.log('Button clicked!');
+  }
 }
+
